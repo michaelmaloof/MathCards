1b9feca67a03cafdf652af5b9e5d891ed19a4567
Michael Maloof 13' Pro <michaelmaloof1991@gmail.com>
2016-04-03 17:04:25 -0300

    Initial Commit

---
+++ MathCards/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MathCards
+//
+//  Created by Michael Maloof on 4/3/16.
+//  Copyright © 2016 Jetpack Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MathCards/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MathCards
+//
+//  Created by Michael Maloof on 4/3/16.
+//  Copyright © 2016 Jetpack Dinosaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
